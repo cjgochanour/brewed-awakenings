a80e3ec0874dfbaf388e7200926b0451660073ae
cjgochanour <cjgochanour@gmail.com>
2021-11-29 15:59:26 -0600

    completed ch5

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,26 @@
-import { getEmployees } from "./database.js";
+import { getEmployees, getOrders } from "./database.js";
 
 const employees = getEmployees();
+const orders = getOrders();
+
+document.addEventListener("click", (event) => {
+    const personClicked = event.target;
+    if (personClicked.id.startsWith("employee")) {
+        const [, personId] = personClicked.id.split("--");
+        let howMany = 0;
+
+        for (const order of orders) {
+            if (order.employeeId === parseInt(personId)) {
+                howMany += 1;
+            }
+        }
+        if (howMany === 1) {
+            window.alert(`${personClicked.innerText} sold ${howMany} product`);
+        } else {
+            window.alert(`${personClicked.innerText} sold ${howMany} products`);
+        }
+    }
+});
 
 export const Employees = () => {
     let html = "<ul>";
